0b8e30004da5082663c2184790763b816657d60b
Thor40 <nicholas.kosik@gmail.com>
2020-08-24 19:19:46 -0500

    resolvers error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User, Book } = require('../models');
+const { User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
@@ -14,15 +14,6 @@ const resolvers = {
             }
 
             throw new AuthenticationError('Not logged in');
-        },
-
-        books: async (parent, { username }) => {
-            const params = username ? { username } : {};
-            return Book.find(params);
-        },
-
-        book: async (parent, { _id }) => {
-            return Book.findOne({ _id });
         }
     },
 
